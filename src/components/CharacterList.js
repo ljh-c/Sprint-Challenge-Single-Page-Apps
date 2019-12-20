3500127eefced17f13d479e28a5de245d1127a2f
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-20 09:59:12 -0800

    button loads a different set of characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,15 +3,17 @@ import axios from "axios";
 import CharacterCard from "./CharacterCard";
 import SearchForm from "./SearchForm";
 import PageBar from "./PageBar";
+import { Button } from "reactstrap";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('');
+  const [sheet, setSheet] = useState(1);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    axios.get('https://rickandmortyapi.com/api/character')
+    axios.get(`https://rickandmortyapi.com/api/character/?page=${sheet}`)
     .then(response => {
       // console.dir(response.data);
       // console.dir(response.data.results);
@@ -22,10 +24,16 @@ export default function CharacterList() {
     .catch(error => {
       console.log('Data not returned', error);
     })
-  }, []);
+  }, [sheet]);
+
+  // * * * GET MORE CHARACTERS
+
+  function getMoreCharacters() {
+    setSheet(sheet + 1);
+  }
 
   // * * * PAGINATION
-  const pageSize = 3;
+  const pageSize = 4;
   const pageCount = Math.ceil(results.length / pageSize);
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -43,6 +51,10 @@ export default function CharacterList() {
     <SearchForm characters={characters} setResults={setResults} query={query} setQuery={setQuery} />
 
     <PageBar handleClick={handleClick} currentPage={currentPage} pageCount={pageCount} />
+
+    <Button color="primary" size="sm" onClick={getMoreCharacters}>
+      More Characters
+    </Button>
 
     <section className="character-list grid-view">
       <h2>Characters</h2>
