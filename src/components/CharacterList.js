4863248e8936f267085a2c8b3828769202e691fd
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-20 08:15:10 -0800

    fetches characters from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      console.dir(response.data);
+      console.dir(response.data.results);
+      console.dir(response.data.results[0]);
+      setCharacters(response.data.results);
+    })
+    .catch(error => {
+      console.log('Data not returned', error);
+    })
   }, []);
 
   return (
